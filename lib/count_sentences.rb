bd298986e4cd37f3f81bef3deb3a1498b519b29e
github username <github email address>
2019-01-30 16:10:28 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,22 @@
 require 'pry'
+require 'rb-readline'
 
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    self.split(/[.!?]/).delete_if{ |sentence| sentence == '' }.count
 
   end
 end
